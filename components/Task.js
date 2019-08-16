f738b018e02100637db3d7f2d814622bfb99e355
p-miszczuk <piotr.miszczuk@interia.pl>
2019-08-16 22:45:00 +0200

    add labels

--- components/Task.js
+++ components/Task.js
@@ -17,13 +17,27 @@ const Task = props => {
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
                 >
+                <div className="trello__card-labels">
+                    <span className="trello__card-label"></span>
+                    <span className="trello__card-label"></span>
+                </div>
                 <span>{props.task.content}</span>
                 <img src="../static/images/pencil.png" className='trello__pencil' />
                 <span className="icon-pencil"></span>
                 <div className="trello__badges">
                     <div className="trello__badge-checklist" title="Elementy listy zadań">
-                        <span className="trello__icon-check"></span>
+                        <img src='../static/images/check.png' className="trello__icon-check" />
                         <span className="trello__info-check"> 1/1 </span>
+                    </div>
+                    <div className="trello__badge-date" title="">
+                        <img src="../static/images/clock.png" className="trello__icon-clock" />
+                        <span className="trello__info-date">14 cze</span>
+                    </div>
+                    <div className="trello__badge-description" title="Ta karta ma opis.">
+                        <span />
+                        <span />
+                        <span />
+                        <span />
                     </div>
                 </div>
                 <style jsx>{`
@@ -70,34 +84,80 @@ const Task = props => {
                         margin: 3px 3px 6px 0;
                         display: flex;
                         flex-direction: row;
+                        align-items: center;
                     }
                     .trello__badge-checklist {
                         padding: 2px 4px 3px;
-                        display: inline;
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center;
                         border-radius: 4px;
                         background-color: #61bd4f;
-                        color: fff; 
+                        color: fff;
+                        margin-right: 3px; 
+                    }
+                    .trello__icon-check {
+                        width: 17px;
+                        height: 17px;
                     }     
                     .trello__info-check {
                         font-size: 12px;
-                        display: inline;
                         font-weight: 400;
                         vertical-align: center;
                         white-space: nowrap;
                         overflow: hidden;
-                    }
-                    .badge-date {
-                        padding: 2px 4px 3px;
-                        display: inline;
-                        border-radius: 4px;
-                        background-color: #eb5a46;
+                        margin-top: 2px;
                         color: #fff;
                     }
-                    .info-date {
+                    .trello__badge-date {
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center;
+                        background-color: #eb5a46;
+                        padding: 2px 4px 3px;
+                        border-radius: 4px;
+                        color: #fff;
+                        margin-right: 3px;
+                    }
+                    .trello__icon-clock {
+                        width: 13px;
+                        height: 13px
+                    }
+                    .trello__info-date {
                         font-size: 12px;
                         font-weight: 400;
                         vertical-align: center;
                         white-space: nowrap;
+                        margin: 2px 0 0 3px;
+                    }
+                    .trello__card-labels {
+                        display: flex;
+                        flex-direction: row;
+                        overflow: auto;
+                    }
+                    .trello__card-label {
+                        display: block;
+                        height: 8px;
+                        margin: 0 4px 4px 0;
+                        width: 40px;
+                        padding: 0;
+                        border-radius: 8px;
+                        position: relative;
+                        z-index: 0;
+                        background-color: yellow;
+                    }
+                    .trello__badge-description {
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-around;
+                        height: 13px;
+                    }
+                    .trello__badge-description span {
+                        display: block;
+                        width: 15px;
+                        height: 1px;
+                        background-color: #000;
+                        overflow: hidden;
                     }
                 `}</style>
             </a>
