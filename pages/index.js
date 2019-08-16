2fe3f30cf635db3322fe5adda304e188ce63d0a2
p-miszczuk <piotr.miszczuk@interia.pl>
2019-08-16 10:22:26 +0200

    inicial settings

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Index = (props) => (
+    <div className="container">
+        Test
+        
+        <style global jsx>{`
+            body {
+                padding: 0px;
+                margin: 0px;
+                box-sizing: border-box;
+            }
+        `}</style>
+    </div>
+)
+
+export default Index;
